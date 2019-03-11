429553294a1027aca999949e78e7537f8753c221
Théophile Candelier <thophilecandelier@yahoo.fr>
2019-03-11 01:50:15 +0100

    Proto
    props spawn and humans spawn cat pushes

---
+++ Assets/Ground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Prop"))
+        {
+            PropManager.instance.PropFallen(other.transform.parent.parent.GetComponent<PropBehaviour>());
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PropManager.instance.PushActiveObject();
+        }
+    }
+
+
+}
